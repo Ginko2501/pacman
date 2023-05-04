2e89d931d7937b5fc6a21426b84ef9888539a8da
Zilu <zl327@cornell.edu>
2023-05-04 19:50:11 -0400

    add match

--- src/voxel.ml
+++ src/voxel.ml
@@ -11,10 +11,23 @@ type t = {
 let make (x : int) (y : int) (s : state) : t = {x; y; s}
 
 (* [plot v] plots the voxel v onto the screen *)
+
 let plot (v : t) =
   match v.s with
-  | _ -> 
+  | Player -> 
       set_color yellow; 
       fill_circle v.x v.y 20;
       set_color black;
-      fill_arc v.x v.y 20 20 (-27) 27;
+      fill_arc v.x v.y 20 20 (-27) 27
+  | Dot -> set_color white; 
+          fill_circle v.x v.y 5
+  | Wall -> set_color blue; 
+            draw_rect (v.x-10) (v.y-10) 20 20;
+  | Empty -> set_color black; 
+            fill_rect (v.x-10) (v.y-10) 20 20
+  | Bot -> set_color red;
+          fill_circle v.x v.y 8;
+          fill_rect (v.x - 8) (v.y - 10) 16 10
+(* [init] initializes the game *)
+
+
